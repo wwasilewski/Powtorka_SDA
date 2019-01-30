a2209ff0cbb93f84b23af17f558b57147108abda
Wojciech <w.wasilewski@gmail.com>
2019-01-30 16:55:38 +0100

    more String using examples

--- src/main/java/sda/powtorka/tydzien1/StringTests.java
+++ src/main/java/sda/powtorka/tydzien1/StringTests.java
@@ -7,10 +7,37 @@ public class StringTests {
         System.out.println("abc");
         String cde = "cde";
         System.out.println("abc" + cde);
-        String c = "abcdef".substring(0,4);
-        String d = cde.substring(1,3);
+        String c = "abcdef".substring(1,4);
         System.out.println(c);
-        System.out.println(d);
         System.out.println(cde.contains("e"));
+
+        System.out.println();
+        String str = "This is my test string";
+        System.out.println(str);
+
+        System.out.println("sixth character is: " + str.charAt(5));
+
+        //counting the occurence of a character in a string
+        int counter = 0;
+
+        for (int i = 0; i < str.length(); i++)
+        {
+            if (str.charAt(i)== 't')
+            {
+                counter++;
+            }
+        }
+        System.out.println("char 't' occured " + counter + " times");
+
+        //equals for checking values of 2 strings and == for checking if they are same object
+        String first = new String("first");
+        String second = new String("first");
+
+        System.out.println(first.equals(second)); //== for same object
+        System.out.println("hashcode of first string: " + first.hashCode());
+        System.out.println("hashcode of second string: " + second.hashCode());
+
+        System.out.println("from 8th index next string is \"my\": " + str.startsWith("my",8));
+        System.out.println("my test string ends with \"test string\": " + str.endsWith("test string"));
     }
 }
