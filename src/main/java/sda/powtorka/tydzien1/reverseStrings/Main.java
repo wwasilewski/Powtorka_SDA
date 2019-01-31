fa74f41b658d061fd51bb4f45232d1e23a680f1f
Wojciech <w.wasilewski@gmail.com>
2019-01-31 17:23:39 +0100

    adding ReverseWords class

---
+++ src/main/java/sda/powtorka/tydzien1/reverseStrings/Main.java
@@ -0,0 +1,9 @@
+package sda.powtorka.tydzien1.reverseStrings;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ReverseWords rs = new ReverseWords();
+        rs.reverseMyStrings("This is my program to reverse every word in a string");
+    }
+}
---
+++ src/main/java/sda/powtorka/tydzien1/reverseStrings/ReverseWords.java
@@ -0,0 +1,20 @@
+package sda.powtorka.tydzien1.reverseStrings;
+
+public class ReverseWords {
+    public void reverseMyStrings(String str) {
+        String words[] = str.split(" ");
+        String reversedString = "";
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            String reverseWord = "";
+
+            for (int j = word.length() - 1; j >= 0; j--) {
+                reverseWord = reverseWord + word.charAt(j);
+            }
+            reversedString = reversedString + reverseWord + " ";
+        }
+        System.out.println(str);
+        System.out.println(reversedString);
+    }
+}
