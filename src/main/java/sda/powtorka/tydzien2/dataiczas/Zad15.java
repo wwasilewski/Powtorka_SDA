89f1d305180daa65c1778d501128a22c1866e11f
Wojciech <w.wasilewski@gmail.com>
2019-02-08 16:06:48 +0100

    small refactoring Zad15 - DateAndTime exercises

--- src/main/java/sda/powtorka/tydzien2/dataiczas/Zad15.java
+++ src/main/java/sda/powtorka/tydzien2/dataiczas/Zad15.java
@@ -11,7 +11,7 @@ import java.util.Locale;
 public class Zad15 {
     public static void main(String[] args) {
         LocalDateTime localDateTime = LocalDateTime.now();
-        System.out.println("localdatetime.now: " + localDateTime);
+        System.out.println("localdatetime.now(): " + localDateTime);
         String polskaData = localDateTime.format(DateTimeFormatter.ofPattern
                 ("d MMMM yyyy 'roku,' EEEE HH:mm:ss", new Locale("pl")));
         System.out.println("po formatowaniu: " + polskaData);
