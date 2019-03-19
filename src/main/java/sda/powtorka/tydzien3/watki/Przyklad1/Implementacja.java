202772ada7b3a8cc2ac8371af45f8a90651f5700
wwasilewski <w.wasilewski@gmail.com>
2019-03-19 13:31:54 +0100

    adding Implementacja class for Threads example

---
+++ src/main/java/sda/powtorka/tydzien3/watki/Przyklad1/Implementacja.java
@@ -0,0 +1,31 @@
+package sda.powtorka.tydzien3.watki.Przyklad1;
+
+/**
+ * @author Wojciech Wasilewski
+ * @date Created on 19.03.2019
+ */
+public class Implementacja implements Runnable {
+
+    Thread t;
+
+    public Implementacja() {
+        t = new Thread(this, "nowy watek");
+        System.out.println("nowy watek: " + t);
+        t.start(); //ta metoda wykonuje metode run()
+    }
+
+
+    @Override //nie musi byc tej adnotacji bo implementuje metode z interfejsu
+    public void run() {
+        try {
+            for (int i = 5; i >= 0; i--) {
+                System.out.println("nowy watek: " + i);
+                Thread.sleep(500);
+            }
+
+        } catch (InterruptedException e) {
+            System.out.println("nowy watek przerwany: " + e);
+        }
+        System.out.println("koniec nowego watku");
+    }
+}
---
+++ src/main/java/sda/powtorka/tydzien3/watki/Przyklad1/ImplementacjaMain.java
@@ -0,0 +1,23 @@
+package sda.powtorka.tydzien3.watki.Przyklad1;
+
+/**
+ * @author Wojciech Wasilewski
+ * @date Created on 19.03.2019
+ */
+public class ImplementacjaMain {
+    public static void main(String[] args) {
+
+        new Implementacja();
+
+        try {
+            for (int i = 5; i >= 0; i--) {
+                System.out.println("watek glowny: " + i);
+                Thread.sleep(1000);
+            }
+
+        } catch (InterruptedException e) {
+            System.out.println("przerwano watek glowny " + e);
+        }
+        System.out.println("koniec watku glownego");
+    }
+}
