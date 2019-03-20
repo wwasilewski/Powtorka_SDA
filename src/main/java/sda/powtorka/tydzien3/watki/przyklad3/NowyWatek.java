447e346addfff4f3d74528dc070ca53992bde1bd
wwasilewski <w.wasilewski@gmail.com>
2019-03-20 12:10:03 +0100

    adding NowyWatek class for Threads example3

---
+++ src/main/java/sda/powtorka/tydzien3/watki/przyklad3/NowyWatek.java
@@ -0,0 +1,30 @@
+package sda.powtorka.tydzien3.watki.przyklad3;
+
+/**
+ * @author Wojciech Wasilewski
+ * @date Created on 20.03.2019
+ */
+public class NowyWatek implements Runnable {
+
+    String nazwa;
+    Thread t;
+
+    public NowyWatek(String nazwaWatku) {
+        nazwa = nazwaWatku;
+        t = new Thread(this, nazwa);
+        System.out.println("nowy watek: " + t);
+        t.start();
+    }
+
+    public void run() {
+        try {
+            for (int i = 5; i >= 0; i--) {
+                System.out.println(nazwa + ": " + i);
+                t.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("przerwano " + nazwa + '\n' + e);
+        }
+        System.out.println("koniec watku: " + nazwa);
+    }
+}
---
+++ src/main/java/sda/powtorka/tydzien3/watki/przyklad3/NowyWatekMain.java
@@ -0,0 +1,22 @@
+package sda.powtorka.tydzien3.watki.przyklad3;
+
+/**
+ * @author Wojciech Wasilewski
+ * @date Created on 20.03.2019
+ */
+public class NowyWatekMain {
+    public static void main(String[] args) {
+
+        new NowyWatek("pierwszy watek");
+        new NowyWatek("drugi watek");
+        new NowyWatek("trzeci watek");
+
+        try {
+            Thread.sleep(8000);
+
+        } catch (InterruptedException e) {
+            System.out.println("przerwano watek glowny" + e);
+        }
+        System.out.println("koniec watku glownego");
+    }
+}
